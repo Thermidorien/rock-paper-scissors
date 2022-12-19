1cc1196a91b66c00546557489492335082b463be
Du-Contrat-Social <gotter.dammerung@outlook.com>
2022-12-19 00:40:47 -0500

    Modified test.js to play RPS for as many wins as demanded

--- test.js
+++ test.js
@@ -26,20 +26,51 @@ function getPlayerSelection() {
 
 function playRound(playerSelection, computerSelection) {
     if (((playerSelection === 'ROCK') && (computerSelection === 'SCISSORS')) || ((playerSelection === 'PAPER') && (computerSelection === 'ROCK')) || ((playerSelection === 'SCISSORS') && (computerSelection === 'PAPER'))) {
+        alert('You won the round');
         return 'VICTORY!';
     }
     else if (((playerSelection === 'SCISSORS') && (computerSelection === 'ROCK')) || ((playerSelection === 'ROCK') && (computerSelection === 'SCISSORS')) || ((playerSelection === 'PAPER') && (computerSelection === 'SCISSORS')))  {
+        alert('You lost the round');
         return 'YOU LOSE!';
     }
     else if (playerSelection === computerSelection) {
+        alert('You drew the round');
         return 'Draw.';
     }
 }
 
-const computerSelection = getComputerChoice();
-var playerSelection = getPlayerSelection();
-console.log(computerSelection);
-console.log(playerSelection);
-console.log(playRound(playerSelection, computerSelection));
-alert(playRound(playerSelection, computerSelection));
+function game() {
+    alert('Let\'s play RPS!');
+    var numberOfWins = prompt('First to how many victories wins the game?','5');
+    var numberOfWins = parseInt(numberOfWins);
+
+    var playerVictories = 0;
+    var computerVictories = 0;
+    
+    while (playerVictories < numberOfWins && computerVictories < numberOfWins) {
+        var computerSelection = getComputerChoice();
+        var playerSelection = getPlayerSelection();
+        var winner = playRound(playerSelection, computerSelection);
+        if (winner === 'VICTORY!') {
+            playerVictories++;
+            console.log('Player wins: ', playerVictories);
+        }
+        else if (winner === 'YOU LOSE!') {
+            computerVictories++;
+            console.log('Computer wins: ', computerVictories);
+        }
+    }
+
+    if (playerVictories > computerVictories) {
+        console.log('Congratulations! YOU WIN!');
+        alert('Congratulations! YOU WIN!');
+    }
+    else if (playerVictories < computerVictories) { 
+        console.log('Sorry! YOU LOSE!');
+        alert('Sorry! YOU LOSE!');
+    }
+}
+
+game();
+
 
